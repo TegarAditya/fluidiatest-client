df4995d3d8198b2e23600fca6a6b2ff4f5189894
Tegar Aditya <tegaraditya53@gmail.com>
2025-01-15 00:08:20 +0700

    refactor test page layout and improve navigation drawer structure

--- pages/test.vue
+++ pages/test.vue
@@ -1,166 +1,168 @@
 <template>
   <div class="relative min-h-[100dvh] w-full bg-blue-100 select-none">
-    <div class="sticky top-0 w-full h-16 z-20 bg-blue-400">
-      <div class="flex justify-between items-center h-full">
-        <ClientOnly>
+    <ClientOnly>
+      <div class="sticky top-0 w-full h-16 z-20 bg-blue-400">
+        <div class="flex justify-between items-center h-full">
           <div></div>
           <h1 class="text-white font-space_grotesk text-lg font-semibold">
             {{ remaingTime }}
           </h1>
           <div></div>
-        </ClientOnly>
+        </div>
       </div>
-    </div>
-    <div
-      ref="el"
-      class="px-5 flex flex-col items-center gap-7 py-10 min-h-[100dvh]"
-    >
       <div
-        class="flex flex-col gap-4 max-w-2xl"
-        v-for="(question, index) in test?.questions"
-        v-show="index === questionIndex"
+        ref="el"
+        class="px-5 flex flex-col items-center gap-7 py-10 min-h-[100dvh]"
       >
-        <Card>
-          <template #title>
-            <h2 class="text-lg font-semibold font-space_grotesk">
-              Soal {{ index + 1 }}
-            </h2>
-          </template>
-          <template #content>
-            <div
-              class="prose prose-p:leading-6 overflow-x-hidden text-justify"
-              v-html="question.question"
-            ></div>
-          </template>
-        </Card>
-        <Panel header="Pilih jawabanmu" class="rounded-xl" toggleable>
-          <div class="grid grid-cols-1 gap-2">
-            <div
-              class="flex items-start gap-2 p-2 border rounded-xl bg-white"
-              v-for="(choice, choiceIndex) in question.options"
-              :key="choiceIndex"
-            >
-              <input
-                type="radio"
-                :id="`${question.id}-${choiceIndex}`"
-                :name="`${question.id}`"
-                :value="choiceIndex"
-                class="mt-2"
-                required
-              />
-              <label
-                class="w-full overflow-x-auto flex items-start gap-2"
-                :for="`${question.id}-${choiceIndex}`"
-              >
-                <div>
-                  <span>{{ choice.label }}.</span>
-                </div>
-                <div v-html="choice.option"></div>
-              </label>
-            </div>
-          </div>
-        </Panel>
-        <div>
-          <Panel
-            header="Pilih alasan jawabanmu"
-            class="rounded-xl"
-            toggleable
-            v-if="test?.isMultiTier"
-          >
+        <div
+          class="flex flex-col gap-4 max-w-2xl"
+          v-for="(question, index) in test?.questions"
+          v-show="index === questionIndex"
+        >
+          <Card>
+            <template #title>
+              <h2 class="text-lg font-semibold font-space_grotesk">
+                Soal {{ index + 1 }}
+              </h2>
+            </template>
+            <template #content>
+              <div
+                class="prose prose-p:leading-6 overflow-x-hidden text-justify"
+                v-html="question.question"
+              ></div>
+            </template>
+          </Card>
+          <Panel header="Pilih jawabanmu" class="rounded-xl" toggleable>
             <div class="grid grid-cols-1 gap-2">
               <div
-                class="flex items-start gap-2 p-2 border rounded-xl bg-white h-full"
-                v-for="(choice, choiceIndex) in question.reasons"
+                class="flex items-start gap-2 p-2 border rounded-xl bg-white"
+                v-for="(choice, choiceIndex) in question.options"
                 :key="choiceIndex"
               >
                 <input
                   type="radio"
-                  :id="`reason-${question.id}-${choiceIndex}`"
-                  :name="`reason-${question.id}`"
+                  :id="`${question.id}-${choiceIndex}`"
+                  :name="`${question.id}`"
                   :value="choiceIndex"
                   class="mt-2"
                   required
                 />
                 <label
                   class="w-full overflow-x-auto flex items-start gap-2"
-                  :for="`reason-${question.id}-${choiceIndex}`"
+                  :for="`${question.id}-${choiceIndex}`"
                 >
                   <div>
                     <span>{{ choice.label }}.</span>
                   </div>
-                  <div v-html="choice.reason"></div>
+                  <div v-html="choice.option"></div>
                 </label>
               </div>
             </div>
           </Panel>
-        </div>
-      </div>
-    </div>
-    <div
-      class="sticky bottom-0 w-full h-16 bg-blue-400 shadow-lg z-10 border-t"
-    >
-      <div class="flex justify-center items-center gap-5 h-full">
-        <Button
-          label="<<"
-          severity="secondary"
-          rounded
-          @click="prevQuestion"
-          :disabled="questionIndex === 0"
-        />
-        <Button
-          severity="secondary"
-          rounded
-          v-show="!isLastQuestion"
-          class="w-32"
-          @click="visibleNavigation = true"
-        >
-          <span>{{ questionIndex + 1 }} dari {{ test?.questions.length }}</span>
-        </Button>
-        <Button
-          label="Selesai"
-          severity="danger"
-          rounded
-          v-show="isLastQuestion"
-          class="w-32 font-semibold"
-        />
-        <Button
-          label=">>"
-          severity="secondary"
-          rounded
-          @click="nextQuestion"
-          :disabled="questionIndex === Number(test?.questions.length) - 1"
-        />
-      </div>
-    </div>
-    <Drawer
-      v-model:visible="visibleNavigation"
-      position="bottom"
-      style="height: auto"
-    >
-      <template #header>
-        <div class="flex items-center gap-2">
-          <span class="font-bold">Amy Elsner</span>
-        </div>
-      </template>
-      <div class="w-full">
-        <div class="grid grid-cols-5 gap-5">
-          <div
-            v-for="(question, index) in test?.questions"
-            :key="index"
-          >
-            <Button
-              :label="String(index + 1)"
-              class="w-8 h-8"
-              :class="{
-                'bg-blue-400 text-white': index === questionIndex,
-                'bg-white text-blue-400': index !== questionIndex,
-              }"
-              @click="questionIndex = index; visibleNavigation = false"
-            />
+          <div>
+            <Panel
+              header="Pilih alasan jawabanmu"
+              class="rounded-xl"
+              toggleable
+              v-if="test?.isMultiTier"
+            >
+              <div class="grid grid-cols-1 gap-2">
+                <div
+                  class="flex items-start gap-2 p-2 border rounded-xl bg-white h-full"
+                  v-for="(choice, choiceIndex) in question.reasons"
+                  :key="choiceIndex"
+                >
+                  <input
+                    type="radio"
+                    :id="`reason-${question.id}-${choiceIndex}`"
+                    :name="`reason-${question.id}`"
+                    :value="choiceIndex"
+                    class="mt-2"
+                    required
+                  />
+                  <label
+                    class="w-full overflow-x-auto flex items-start gap-2"
+                    :for="`reason-${question.id}-${choiceIndex}`"
+                  >
+                    <div>
+                      <span>{{ choice.label }}.</span>
+                    </div>
+                    <div v-html="choice.reason"></div>
+                  </label>
+                </div>
+              </div>
+            </Panel>
           </div>
         </div>
       </div>
-    </Drawer>
+      <div
+        class="sticky bottom-0 w-full h-16 bg-blue-400 shadow-lg z-10 border-t"
+      >
+        <div class="flex justify-center items-center gap-5 h-full">
+          <Button
+            label="<<"
+            severity="secondary"
+            rounded
+            @click="prevQuestion"
+            :disabled="questionIndex === 0"
+          />
+          <Button
+            severity="secondary"
+            rounded
+            v-show="!isLastQuestion"
+            class="w-32"
+            @click="visibleNavigation = true"
+          >
+            <span
+              >{{ questionIndex + 1 }} dari {{ test?.questions.length }}</span
+            >
+          </Button>
+          <Button
+            label="Selesai"
+            severity="danger"
+            rounded
+            v-show="isLastQuestion"
+            class="w-32 font-semibold"
+          />
+          <Button
+            label=">>"
+            severity="secondary"
+            rounded
+            @click="nextQuestion"
+            :disabled="questionIndex === Number(test?.questions.length) - 1"
+          />
+        </div>
+      </div>
+      <Drawer
+        v-model:visible="visibleNavigation"
+        position="bottom"
+        style="height: auto"
+      >
+        <template #header>
+          <div class="flex items-center gap-2">
+            <span class="font-bold">Amy Elsner</span>
+          </div>
+        </template>
+        <div class="w-full">
+          <div class="grid grid-cols-5 gap-5">
+            <div v-for="(question, index) in test?.questions" :key="index">
+              <Button
+                :label="String(index + 1)"
+                class="w-8 h-8"
+                :class="{
+                  'bg-blue-400 text-white': index === questionIndex,
+                  'bg-white text-blue-400': index !== questionIndex,
+                }"
+                @click="
+                  questionIndex = index,
+                  visibleNavigation = false
+                "
+              />
+            </div>
+          </div>
+        </div>
+      </Drawer>
+    </ClientOnly>
   </div>
 </template>
 
